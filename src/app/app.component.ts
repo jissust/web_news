4294b366449bba5355cc2ba919550ae73c8e7493
jesustissera <jesustissera@gmail.com>
2024-04-29 17:46:05 -0300

    Agrego bloque de videos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ResultAndPositionComponent } from './pages/result-and-position/result-and-position.component';
 import { OutstandingNewsComponent } from './pages/outstanding-news/outstanding-news.component';
+import { VideosComponent } from './pages/videos/videos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HomeComponent, ResultAndPositionComponent, OutstandingNewsComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, ResultAndPositionComponent, OutstandingNewsComponent, VideosComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
