05f2bbf92e266ca07a7e5cce692cf3f484672229
jesustissera <jesustissera@gmail.com>
2024-05-02 00:20:33 -0300

    front para la noticia

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { NoticesComponent } from './pages/notices/notices.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NoticeComponent } from './pages/notices/components/notice/notice.component';
 
 export const routes: Routes = [
     {path:'', component: HomeComponent},
     {path:'notices', component: NoticesComponent},
+    {path:'notice/:id', component: NoticeComponent}
 ];
--- src/app/pages/notices/notices.component.ts
+++ src/app/pages/notices/notices.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NewService } from '../../services/new.service';
+import { ItemNewsComponent } from '../../components/item-news/item-news.component';
+import { NoticeComponent } from './components/notice/notice.component';
 
 @Component({
   selector: 'app-notices',
   standalone: true,
-  imports: [],
+  imports: [ItemNewsComponent, NoticeComponent],
   templateUrl: './notices.component.html',
   styleUrl: './notices.component.css'
 })
-export class NoticesComponent {
+export class NoticesComponent implements OnInit{
+  public news!: any;
+  constructor(private _new: NewService){
 
+  }
+  ngOnInit(): void {
+      this.news = this._new.getItems();
+      console.log(this._new);
+  }
 }
